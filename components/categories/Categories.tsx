f05d2b4cc298545d0395e97d40de3165cd02e9ae
Mammadali Gasimov <mamed-gasimov@Mamed.local>
2022-03-21 16:03:12 +0400

    fix try

--- components/categories/Categories.tsx
+++ components/categories/Categories.tsx
@@ -4,7 +4,7 @@ import { getCategories } from '../../services/sendGraphRequest';
 import { CategoryType } from '../../types/CategoryType';
 
 const Categories: FC = () => {
-    const [categories, setCategories] = useState<CategoryType[]>([]);
+    const [categories, setCategories] = useState<CategoryType[]>();
 
     useEffect(() => {
         getCategories()
@@ -16,7 +16,7 @@ const Categories: FC = () => {
     return (
         <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
             <h3 className="text-xl mb-8 font-semibold border-b pb-4">Categories</h3>
-            {categories.map((category, index) => (
+            {categories?.map((category, index) => (
                 <Link key={index} href={`/category/${category.slug}`}>
                     <span className={
                         `cursor-pointer block ${(index === categories.length - 1) ? 'border-b-0' : 'border-b'} pb-3 mb-3`
